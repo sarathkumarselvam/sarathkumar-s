2a80108058ed2647684e9ecb43e2421b0c84266d
sarathkumarselvam <47300048+sarathkumarselvam@users.noreply.github.com>
2019-06-15 00:03:18 +0530

    Create vowelsornot.py

---
+++ vowelsornot.py
@@ -0,0 +1,6 @@
+ch = input("enter the ch:")
+if(ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u" or ch=="A" or ch=="E" or
+   ch=="I" or ch=="o" or ch=="u"):
+    print("ch is vowels")
+else:
+    print("constant")
