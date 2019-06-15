c431e1b3c5cf2974489f0c70c8b58148d8a4e88d
sarathkumarselvam <47300048+sarathkumarselvam@users.noreply.github.com>
2019-06-15 23:52:26 +0530

    Create gretr.py

---
+++ gretr.py
@@ -0,0 +1,9 @@
+a=float(input("enter the value:"))
+b=float(input("enter the value:"))
+c=float(input("enter the value:"))
+if (a>b and a>c):
+        print("a is gretaer")
+elif (b>a and b>c):
+        print("b is greater")
+elif (c>a and c>b):
+        print("c is greater")
